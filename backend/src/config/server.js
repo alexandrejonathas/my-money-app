d3b3b24624bc4681aad8e448617695eb14a53a25
Jonathas Lima <alexandrejonathas@gmail.com>
2017-06-13 12:54:47 -0300

    Comitando o projeto com backend finalizado

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
 
 const port = 3003
 server.listen(port, function(){
