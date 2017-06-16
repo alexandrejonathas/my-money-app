78a5c4276fe13b4a868e5f4be34e51473d55ae1e
Jonathas Lima <alexandrejonathas@gmail.com>
2017-06-16 12:51:44 -0300

    Comitando o projeto com devTools configurado

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -6,20 +8,27 @@ import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
+import { getSummary } from './dashboardActions'
+
 class Dashboard extends Component {
 
+    componentWillMount() {
+        this.props.getSummary()
+    }
+
     render(){
+        const {credit, debt } = this.props.summary
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
                     <Row>
                         <ValueBox cols='12 4' color='green' icon='bank'
-                            value='$10' text='Total de créditos' />
+                            value={`R$ ${credit}`} text='Total de créditos' />
                         <ValueBox cols='12 4' color='red' icon='credit-card'
-                            value='$10' text='Total de débitos' />
+                            value={`R$ ${debt}`} text='Total de débitos' />
                         <ValueBox cols='12 4' color='blue' icon='money'
-                            value='$0' text='Valor consolidado' />
+                            value={`R$ ${credit - debt}`} text='Valor consolidado' />
                     </Row>                                                
                 </Content>
             </div>
@@ -27,4 +36,8 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
