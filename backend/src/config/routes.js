8671fbc49b76878d6aeb00917aee0e54073483c0
Jonathas Lima <alexandrejonathas@gmail.com>
2017-07-14 13:00:44 -0300

    Comitando o projeto com authReducer criado falta adicionar ao rootReducer

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server){
-
+    
     //Definir url base
-    const router = express.Router()
-    server.use('/api', router)
+    /*
+    * Rotas protejidas por token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     //Rotas do ciclo de pagamento
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signUp)
+    openApi.post('/validate-token', AuthService.validateToken)    
+
 }
