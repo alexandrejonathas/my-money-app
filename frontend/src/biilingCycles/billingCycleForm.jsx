57402217cac3b5b0b64733a325ca73505fcb9ce5
Jonathas Lima <alexandrejonathas@gmail.com>
2017-07-14 10:08:45 -0300

    Comitando o projeto sem autenticação

--- frontend/src/biilingCycles/billingCycleForm.jsx
+++ frontend/src/biilingCycles/billingCycleForm.jsx
@@ -13,8 +13,18 @@ import Summary from './summary'
 
 class BillingCycleForm extends Component{
     
+    calculateSummary(){
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
     render(){
         const { handleSubmit, readOnly, credits, debts } = this.props
+        const {sumOfCredits, sumOfDebts} = this.calculateSummary()
+
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
@@ -27,7 +37,7 @@ class BillingCycleForm extends Component{
                     <Field name='year' component={LabelAndInput} 
                         readOnly={readOnly}
                         label='Ano' type='number' cols='12 4' placeholder='Informe o ano' />
-                    <Summary credit={1000} debt={100} />    
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />    
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} 
                         field='credits' legend='Créditos'/>
                     <ItemList cols='12 6' list={debts} readOnly={readOnly} 
